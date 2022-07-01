16cde4a8818512402c0cdd4219dfdf46045477db
Cherica Wallace <cwalla23@hotmail.com>
2022-07-01 13:49:50 -0400

    Add computerplay function

---
+++ script.js
@@ -0,0 +1,11 @@
+"strict mode";
+
+// let userScore = 0;
+// let computerScore = 0;
+
+function computerPlay() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomResult = Math.floor(Math.random() * 3);
+  return choices[randomResult];
+}
+computerPlay();
