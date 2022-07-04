ef3576b117cc3301b620979f764b6d04dd8615a4
Cherica Wallace <cwalla23@hotmail.com>
2022-07-04 13:01:28 -0400

    Add multi-round gameRound function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "strict mode";
 
-// let userScore = 0;
+// let playerScore = 0;
 // let computerScore = 0;
 
 function computerPlay() {
@@ -9,4 +9,26 @@ function computerPlay() {
   return choices[randomResult];
 }
 
-console.log(computerPlay());
+// console.log(computerPlay());
+
+function gameRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) return "You Tie!";
+  // PLAYER LOSE
+  else if (playerSelection === "Rock" && computerSelection === "Paper")
+    return "You Lose! Paper beats Rock";
+  else if (playerSelection === "Paper" && computerSelection === "Scissors")
+    return "You Lose! Scissors beats Paper";
+  else if (playerSelection === "Scissors" && computerSelection === "Rock")
+    return "You Lose! Rock beats Scissors";
+  //PLAYER WINS
+  else if (playerSelection === "Paper" && computerSelection === "Rock")
+    return "You Win! Paper beats Rock";
+  else if (playerSelection === "Scissors" && computerSelection === "Paper")
+    return "You Win! Scissors beats Paper";
+  else playerSelection === "Rock" && computerSelection === "Scissors";
+  return "You Win! Rock beats Scissors";
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log(gameRound(playerSelection, computerSelection));
