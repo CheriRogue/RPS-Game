4d56b24c8f9982bdd6ac7f72b83a7d6dcb6072e2
Cherica Wallace <cwalla23@hotmail.com>
2022-07-09 12:24:31 -0400

    Improve readability with switch statement

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
 
-// // let playerScore = 0;
-// // let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 const choices = ["Rock", "Paper", "Scissors"];
 
-let playerSelection = "Rock";
+let playerSelection = prompt("Rock, Paper, Scissors?");
+
 const computerSelection = computerPlay();
 
 function computerPlay() {
@@ -14,23 +15,30 @@ function computerPlay() {
   return choices[randomResult];
 }
 
-console.log(computerPlay());
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) return "You Tie!";
-  else if (playerSelection === "Paper" && computerSelection === "Rock")
-    return "You Win! Paper beats Rock";
-  else if (playerSelection === "Scissors" && computerSelection === "Paper")
-    return "You Win! Scissors beats Paper";
-  else if (playerSelection === "Rock" && computerSelection === "Scissors")
-    return "You Win! Rock beats Scissors";
-  else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+function playRound(playerSelection) {
+  const computerSelection = computerPlay();
+  console.log(`user choice => ${playerSelection}`);
+  console.log(`computer choice => ${computerSelection}`);
+  switch (playerSelection + computerSelection) {
+    case "PaperRock":
+    case "ScissorsPaper":
+    case "RockScissors":
+      playerScore++;
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    case "ScissorsRock":
+    case "RockPaper":
+    case "PaperScissors":
+      computerScore++;
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    case "ScissorsScissors":
+    case "RockRock":
+    case "PaperPaper":
+      return `You tied! Both chose ${playerSelection}`;
   }
 }
 
 function game() {
-  return playRound(playerSelection, computerSelection);
+  for (let i = 0; i < 5; i++) return playRound(playerSelection);
 }
 
 console.log(game());
